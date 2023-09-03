be8e59341c736db3a9db286e773398ba7486c77a
Yehonatan Koritny <yonatan.koritny@gmail.com>
2023-09-03 15:13:12 +0300

    Gameplay fixes
    And added the house camera movement

--- Assets/Libs/Logic/Scripts/GameManager.cs
+++ Assets/Libs/Logic/Scripts/GameManager.cs
@@ -1,94 +1,107 @@
-using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Rendering;
 
+// Use Unity's Random instead of System's
 using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
-    
+    // Public variables
     public GameObject meteorPrefab;
     public GameObject player;
-    public const int maxMeteors = 10;
-
+    public const int maxMeteors = 20;
     public float cameraXHouseScreen = -13.63f;
 
+    // Private variables
     private Sprite[] meteorSprites;
-
     private Camera cam;
-    private BoxCollider2D LeftScreenTrigger;
-
     private int meteorCount = 0;
+    private bool cameraOnHouse = false;
+    private Vector3 leftCameraLocation;
+    private Vector3 rightCameraLocation;
+    private Coroutine cameraMovment;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
-        LeftScreenTrigger = GetComponentInChildren<BoxCollider2D>();
 
-        // Load all sprites named "meteor_1" to "meteor_6" from the Resources folder
+        // Initialize camera locations
+        leftCameraLocation = new Vector3(cameraXHouseScreen, 0, -10);
+        rightCameraLocation = cam.transform.position;
+
+        // Load meteor sprites from Resources folder
         meteorSprites = new Sprite[6];
         for (int i = 1; i <= 6; i++)
         {
             meteorSprites[i - 1] = Resources.Load<Sprite>("Meteor_" + i);
         }
-        StartCoroutine (SpawnMeteors());
-        // StartCoroutine(PlayerLeftScreen()); // Uncomment to watch camera move to left
 
-
+        StartCoroutine(SpawnMeteors());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() { } // If Update is not required, it can be removed
 
+    // Coroutine to spawn meteors
     IEnumerator SpawnMeteors()
     {
         while (true)
         {
             if (meteorCount < maxMeteors)
             {
-                // Instantiate the new Meteor object from the prefab
-                var newMeteor = Instantiate(meteorPrefab);
-
-                // Get the SpriteRenderer component from the new instantiated object
+                GameObject newMeteor = Instantiate(meteorPrefab);
                 SpriteRenderer spriteRenderer = newMeteor.GetComponent<SpriteRenderer>();
-
-                // Get a random sprite from the loaded sprites
-                Sprite randomSprite = meteorSprites[Random.Range(0, meteorSprites.Length)];
-
-                // Assign the random sprite to the instantiated object
-                spriteRenderer.sprite = randomSprite;  // This line should refer to spriteRenderer, not meteorPrefab
-
-                // Optionally, set the sorting order if you want to bring it to the front
-                spriteRenderer.sortingOrder = 1;
-
-                // Increment meteorCount
+                spriteRenderer.sprite = meteorSprites[Random.Range(0, meteorSprites.Length)];
+                spriteRenderer.sortingOrder = 1; // Bring to the front
                 meteorCount++;
             }
             yield return new WaitForSeconds(Random.value);
         }
     }
 
-
+    // Handle the meteor destroyed event
     public void MeteorDestroyed(MeteorBehaviours meteor)
     {
         meteorCount--;
     }
 
-    private IEnumerator PlayerLeftScreen()
+    // Handle player exiting the screen to the left
+    public void PlayerLeftScreen()
     {
-        var i = 0f;
-        while (i < 1 )
+        if (!cameraOnHouse)
         {
-            cam.transform.position = Vector3.Lerp(cam.transform.position, new Vector3(cameraXHouseScreen, 0, -10), i);
-            i += 0.005f * Time.deltaTime;
+            if (cameraMovment != null)
+            {
+                StopCoroutine(cameraMovment);
+            }
+            cameraMovment = StartCoroutine(MoveCamera(leftCameraLocation));
+            cameraOnHouse = true;
+        }
+    }
+
+    // Handle player re-entering the game
+    public void PlayerReenterGame()
+    {
+        if (cameraOnHouse)
+        {
+            if (cameraMovment != null)
+            {
+                StopCoroutine(cameraMovment);
+            }
+            cameraMovment = StartCoroutine(MoveCamera(rightCameraLocation));
+            cameraOnHouse = false;
+        }
+    }
+
+    // Coroutine to move the camera
+    private IEnumerator MoveCamera(Vector3 targetPosition)
+    {
+        float i = 0f;
+        Vector3 curCameraPosition = cam.transform.position;
+        while (i < 1)
+        {
+            cam.transform.position = Vector3.Lerp(curCameraPosition, targetPosition, i);
+            i += 0.5f * Time.deltaTime;
             yield return null;
         }
-
     }
 }
--- Assets/Libs/Logic/Scripts/Player/PlayerMovement.cs
+++ Assets/Libs/Logic/Scripts/Player/PlayerMovement.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     private bool _jumping = false;
     private Rigidbody2D _rigidbody;
+    private GameManager gameManager;
+
+   
 
     private void Walking(float dirX)
     {
@@ -15,12 +20,13 @@ public class PlayerMovement : MonoBehaviour
     private void Jumping()
     {
         // _rigidbody.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
-        _rigidbody.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+        _rigidbody.AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
         _jumping = false;
     }
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
     
     private void Update()
@@ -42,5 +48,19 @@ public class PlayerMovement : MonoBehaviour
 
         Jumping();
 
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.gameObject.name);
+        if (collision.gameObject.name == "PlayerLeftScreenTrigger")
+        {
+            gameManager.PlayerLeftScreen();
+        }
+        else if (collision.gameObject.name == "PlayerRightScreenTrigger")
+        {
+            gameManager.PlayerReenterGame();
+        }
+        
     }
 }
